d13b146739cbc59dd087aad95a5bc0a75c691b73
omphilejmatsobe <omphilejmatsobe@gmail.com>
2025-11-10 18:39:38 +0200

    Feat: Add, Edit and Delete function implemented.

--- src/todo.ts
+++ src/todo.ts
@@ -6,26 +6,78 @@ interface taskItem
 
 let size = 0
 let tasks:taskItem[] = []
+
+
 let addbutton = document.querySelector("button")
+let tasksComps = document.querySelector("ul")
 let input = document.querySelector("input")
 
-function addTask (taskText: string)
+function addTask (e:Event)
 {
+
+    e.preventDefault()
+
+    if (input!.value == "")
+    {
+        alert("Add Task")
+        return
+    }
+
     let newTaskItem: taskItem = 
     {
-        task: taskText,
+        task: input!.value,
         id: size
     }
+
+
     size++;
     tasks.push(newTaskItem);
+
+    let listItem = document.createElement('li')
+
+    // Input Text
+    let text = document.createElement('span');
+    text.textContent = newTaskItem.task;
+
+    // Edit button
+    let edit_button = document.createElement('button')
+    edit_button.textContent = 'Edit'
+
+    // Delete button
+    let delete_button = document.createElement('button')
+    delete_button.textContent = "Delete"
+
+    edit_button.addEventListener("click", editTask)
+    delete_button.addEventListener("click", deleteTask)
+
+    listItem.appendChild(text)
+    listItem.appendChild(edit_button)
+    listItem.appendChild(delete_button)
+    tasksComps?.appendChild(listItem)
+    input!.value = ''
 }
 
-function editTask (id:number, newText:string)
+function editTask (e:Event)
 {
-    tasks[id].task = newText
+    let target = e.target as HTMLElement
+    let listItem = target.parentElement
+    let text = listItem?.querySelector('span')
+
+    let newText = prompt('Edit task:', text ? text.textContent : '')
+    
+    if (newText == '')
+    {
+        alert("No text entered.")
+        return
+    }
+
+    text!.textContent = newText;  
 }
 
-function deleteTask (id: number)
+function deleteTask (e:Event)
 {
-    tasks.splice(id,id)
+    console.log("Hi")
+    let target = e.target as HTMLButtonElement
+    let listItem = target.parentElement
+    tasksComps?.removeChild(listItem!)
 }
