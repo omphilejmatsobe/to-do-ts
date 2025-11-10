f2367fa1021f29a76d81e652e7a493851f0163c2
omphilejmatsobe <omphilejmatsobe@gmail.com>
2025-11-10 19:19:45 +0200

    Feat: Screenshots added

--- dist/todo.js
+++ dist/todo.js
@@ -1,21 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let size = 0;
-let tasks = [];
-let addbutton = document.querySelector("button");
+let tasksComps = document.querySelector("ul");
 let input = document.querySelector("input");
-function addTask(taskText) {
+function addTask(e) {
+    e.preventDefault();
+    if (input.value == "") {
+        alert("Add Task");
+        return;
+    }
     let newTaskItem = {
-        task: taskText,
-        id: size
+        text: document.createElement('span'),
+        editButton: document.createElement('button'),
+        deleteButton: document.createElement('button'),
     };
-    size++;
-    tasks.push(newTaskItem);
+    let listItem = document.createElement('li');
+    newTaskItem.text.textContent = input.value;
+    newTaskItem.editButton.textContent = 'Edit';
+    // Delete button
+    let delete_button = document.createElement('button');
+    delete_button.textContent = "Delete";
+    newTaskItem.editButton.addEventListener("click", editTask);
+    newTaskItem.deleteButton.addEventListener("click", deleteTask);
+    listItem.appendChild(newTaskItem.text);
+    listItem.appendChild(newTaskItem.editButton);
+    listItem.appendChild(newTaskItem.deleteButton);
+    tasksComps?.appendChild(listItem);
+    input.value = '';
 }
-function editTask(id, newText) {
-    tasks[id].task = newText;
+function editTask(e) {
+    let target = e.target;
+    let listItem = target.parentElement;
+    let text = listItem?.querySelector('span');
+    let newText = prompt('Edit task:', text ? text.textContent : '');
+    if (newText == '') {
+        alert("No text entered.");
+        return;
+    }
+    text.textContent = newText;
 }
-function deleteTask(id) {
-    tasks.splice(id, id);
+function deleteTask(e) {
+    console.log("Hi");
+    let target = e.target;
+    let listItem = target.parentElement;
+    tasksComps?.removeChild(listItem);
 }
 //# sourceMappingURL=todo.js.map
